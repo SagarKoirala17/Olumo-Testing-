73402424f72470ffec596372082ac2d5b14e588f
Sagar Koirala <koiralasagar138@gmail.com>
2023-03-13 14:44:07 +0545

    category

---
+++ cypress/category.js
@@ -0,0 +1,35 @@
+export const campaignCategories = [
+    "Awareness campaigns",
+    "Advocacy campaigns",
+    "Fundraising campaigns",
+    "Political campaigns",
+    "Corporate social responsibility campaigns",
+    "Product launch campaigns",
+    "Crisis management campaigns",
+    "Employee engagement campaigns",
+    "Diversity and inclusion campaigns"
+  ];
+  export const categoryDescriptions = [
+    "Campaigns designed to raise awareness and educate the public about a particular issue or topic.",
+    "Campaigns designed to persuade individuals or groups to take action on a particular issue or cause.",
+    "Campaigns designed to raise funds for a particular cause, organization, or project.",
+    "Campaigns designed to promote a political candidate or party and encourage voter turnout.",
+    "Campaigns designed to showcase a company's commitment to social and environmental responsibility.",
+    "Campaigns designed to promote a new product or service and generate interest and sales.",
+    "Campaigns designed to manage a crisis or negative event and maintain or restore a positive public image.",
+    "Campaigns designed to engage and motivate employees and improve workplace culture.",
+    "Campaigns designed to promote diversity and inclusion in the workplace or society at large."
+  ];
+  
+  export const categoryIntendedResults = [
+    "Increased public awareness and understanding of the issue or topic.",
+    "Increased engagement and action on the issue or cause.",
+    "Increased funds to support the cause, organization, or project.",
+    "Successful election of the candidate or promotion of the political party.",
+    "Improved public perception of the company's social and environmental responsibility.",
+    "Increased sales or adoption of the product or service.",
+    "Effective management of the crisis or negative event.",
+    "Improved employee engagement, productivity, and job satisfaction.",
+    "Improved diversity and inclusion in the workplace or society at large."
+  ];
+  
