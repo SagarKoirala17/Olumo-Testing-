dd4a6fb0947acc28f79280515413f00a0a7f0a1f
Sagar Koirala <koiralasagar138@gmail.com>
2023-07-07 13:18:19 +0545

    employee portal completion

--- cypress/e2e/employee_portal/employee/employee.js
+++ cypress/e2e/employee_portal/employee/employee.js
@@ -6,14 +6,59 @@ Cypress.Commands.add('FetchScaleQuestionArray',()=>{
       array.push(id);
     }).then(() => {
       console.log(array);
-       window.filteredArray = array.filter((element) => element !== undefined);
+      window.filteredArray = array.filter((element) => element !== undefined);
       window.filteredArrays = filteredArray.filter((element) => !element.includes('feedback'));
       console.log(filteredArrays)
     });
 })
-Cypress.Commands.add('ScaleQuestionResponse',()=>{
+Cypress.Commands.add('FetchOnlyNumber',()=>{
     console.log(filteredArrays)
-    
+     window.numberArray = filteredArrays.map((element) => parseInt(element.match(/\d+/)[0]));
+    console.log(numberArray)
+})
+Cypress.Commands.add('AnswerQuestions', () => {
+  window.numberArray.forEach((number) => {
+    let multipleChoiceSelector = `#edit_response_${number} > .vertical-answers > :nth-child(1) > .ui > .collection_radio_buttons`;
+    let scaleClassSelector = `#edit_response_${number} > .horizontal-answers > :nth-child(1) > .ui > .collection_radio_buttons`;
+    let freeResponseSelector = `#response-${number}-feedback`;
+
+  })
+})
+
+
+function getRandomValue(number) {
+  return Math.floor(Math.random() * number) + 1; // Generates a random value between 1 and 5
+}
+
+Cypress.Commands.add('AnswerQuestions', () => {
+  window.numberArray.forEach((number) => {
+    let randomChoice = getRandomValue(2);
+    let randomValue=getRandomValue(5)
     
 
-})
+    let multipleChoiceSelector = `#edit_response_${number} > .vertical-answers > :nth-child(${randomChoice}) > .ui > .collection_radio_buttons`;
+    let scaleClassSelector = `#edit_response_${number} > .horizontal-answers > :nth-child(${randomValue}) > .ui > .collection_radio_buttons`;
+    let freeResponseSelector = `#response-${number}-feedback`;
+
+    // Perform actions specific to multiple choice question
+    if (Cypress.$(multipleChoiceSelector).length) {
+      cy.get(multipleChoiceSelector)
+        .click(); // Example action: click on the randomly selected option
+      return; // Exit the current loop iteration
+    }
+
+    // Perform actions specific to scale class question
+    if (Cypress.$(scaleClassSelector).length) {
+      cy.get(scaleClassSelector)
+        .click(); // Example action: click on the randomly selected option
+      return; // Exit the current loop iteration
+    }
+
+    // Perform actions specific to free response question
+    if (Cypress.$(freeResponseSelector).length) {
+      cy.get(freeResponseSelector)
+        .type('Sample text'); // Example action: type the randomly selected value
+    }
+  });
+});
+
--- cypress/e2e/employee_portal/unanswered_responses.cy.js
+++ cypress/e2e/employee_portal/unanswered_responses.cy.js
@@ -3,7 +3,11 @@ describe('Answer the Unanswered Responses', () => {
     it('Answer the unanswered response', () => {
       cy.visit('/me/unanswered?token=xJJ9PSrQoCRufPM3endxSYmS');
       cy.FetchScaleQuestionArray()
-      cy.ScaleQuestionResponse()
+      cy.FetchOnlyNumber()
+      cy.AnswerQuestions()
+      //Click the Submit Button
+      cy.get('#submit').should('be.visible').click()
+      cy.get('.twelve > img').should('be.visible')
     });
   });
   
